e6312e1e1cdf0fcc6d09f81f0c576a939959226d
Hansel <valkirecesa@gmail.com>
2021-08-31 16:36:04 +0800

    fix: My Cart - edited the select qty input - edited customjs file for reduce, increase, checkqty

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -1,25 +1,29 @@
 function reduceQty(btn, id) {
     // id is => item-1, item-2, item-3 and so on
     let input_field = document.querySelector(`#${id}`);
-    
+
+    input_field.value--;
     if (input_field.value <= 1) {
+        input_field.value = 1;
         btn.classList.add('disabled');
         return;
     }
-    
+
     // add btn is => add-item-1, add-item-2, add-item-3 and so on
     let add_btn = document.querySelector(`#add-${id}`);
     add_btn.classList.remove('disabled');
 
-    input_field.value--;
 }
 
 function increaseQty(btn, id) {
     // id is => item-1, item-2, item-3 and so on
     let input_field = document.querySelector(`#${id}`);
 
+    input_field.value++;
+
     // if qty is over max qty of item then just disabled
     if (input_field.value >= 20) {
+        input_field.value = 20;
         btn.classList.add('disabled');
         return;
     }
@@ -27,23 +31,29 @@ function increaseQty(btn, id) {
     // reduce btn is => reduce-item-1, reduce-item-2, reduce-item-3 and so on
     let reduce_btn = document.querySelector(`#reduce-${id}`);
     reduce_btn.classList.remove('disabled');
-
-    input_field.value++;
 }
 
 function checkQty(input, reduce_btn_id, add_btn_id) {
     let reduce_btn = document.querySelector(`#${reduce_btn_id}`);
     let add_btn = document.querySelector(`#${add_btn_id}`);
 
-
+    
     if (input.value == "" || input.value == 0) {
         input.value = 1;
         return;
     }
+
+    // if somewhere in between
+    if (input.value > 1 && input.value < 20) {
+        reduce_btn.classList.remove('disabled');
+        add_btn.classList.remove('disabled');
+        return;        
+    }
     // if qty is lesser than 1, then disable reduce
     if (input.value <= 1) {
+        input.value = 1;
         reduce_btn.classList.add('disabled');
-        return;
+        add_btn.classList.remove('disabled');
     }
 
     // if qty is greater than max qty of an item, then disable add
@@ -54,10 +64,5 @@ function checkQty(input, reduce_btn_id, add_btn_id) {
 
         reduce_btn.classList.remove('disabled');
         add_btn.classList.add('disabled');
-        return;
     }
-
-    // if somewhere in between
-    reduce_btn.classList.remove('disabled');
-    add_btn.classList.remove('disabled');
 }
